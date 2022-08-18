c9d2b02ab2c196bc65b82a256184b1dbf157edb3
Ian R. Higgins <ianrhiggins@gmail.com>
2022-08-18 15:42:32 -0700

    updated select statement

--- app.py
+++ app.py
@@ -95,9 +95,12 @@ def reference_informationn_get_options(value):
         cur.execute(f"select datum from sites where site_number = '{value}'")
             #df = pd.DataFrame(cur.fetchall())
         df = pd.DataFrame(cur.fetchall(),columns=['datum'])
-        df = df["datum"].to_list()
-        #df = Parameters.columns.values.tolist()
-        return [{'label': i, 'value': i} for i in df]
+        df = df["site_number"].to_list()
+        if df[0] == "None":
+            df[0] = "site exists; no information given"
+            return [{'label': i, 'value': i} for i in df]
+        else:
+            return [{'label': i, 'value': i} for i in df]
         #return df
     
 #
