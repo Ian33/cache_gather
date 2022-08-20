cb0890b8790fd24f6ea82d609b8f8434e2ef50f6
Ian R. Higgins <ianrhiggins@gmail.com>
2022-08-20 14:08:37 -0700

    updated reference elevation input

--- get_site_list.py
+++ get_site_list.py
@@ -4,11 +4,19 @@ import psycopg2
 import pandas as pd
 
 def query_site_list():
-    DATABASE_URL = os.environ['DATABASE_URL']
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    except:
+        username = "cwtcmzqujpmszj"
+        password = "d9717d1ad9420277ea4b7a2332885a6e7cbc39073d29a0cfd9f733d2df6835b6"
+        host_name = "ec2-44-206-197-71.compute-1.amazonaws.com"
+        db_name = "d3fpg63d3qj0lt"
+        conn = psycopg2.connect(f"postgresql://{username}:{password}@{host_name}/{db_name}")
     cur = conn.cursor()
     cur.execute("select site_number from sites")
     #df = pd.DataFrame(cur.fetchall())
     df = pd.DataFrame(cur.fetchall(),columns=['site_number'])
     df = df["site_number"].to_list()
+    
     return(df)
--- reference_information.py
+++ reference_information.py
@@ -5,26 +5,45 @@ import pandas as pd
 
 def get_reference_information(value):
     if value == "":
-        return [{'label': "", 'value': ""}]
-    else:
-        DATABASE_URL = os.environ['DATABASE_URL']
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cur = conn.cursor()
-        cur.execute(f"select reverence_information from sites where site_number = '{value}'")
-        df = pd.DataFrame(cur.fetchall(),columns=['reverence_information'])
-        df = df["reverence_information"].tolist()
-        return [{'label': i, 'value': i} for i in df]
-        #return df
+        return "", "", "", "select site"
 
-def get_reference_elevation(value):
-    if value == "":
-        return "blank"
     else:
-        DATABASE_URL = os.environ['DATABASE_URL']
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        try:
+            DATABASE_URL = os.environ['DATABASE_URL']
+            conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        except:
+            username = "cwtcmzqujpmszj"
+            password = "d9717d1ad9420277ea4b7a2332885a6e7cbc39073d29a0cfd9f733d2df6835b6"
+            host_name = "ec2-44-206-197-71.compute-1.amazonaws.com"
+            db_name = "d3fpg63d3qj0lt"
+            conn = psycopg2.connect(f"postgresql://{username}:{password}@{host_name}/{db_name}")
         cur = conn.cursor()
-        cur.execute(f"select reverence_elevation from sites where site_number = '{value}'")
-        df = pd.DataFrame(cur.fetchall(),columns=['reverence_elevation'])
-        df = df["reverence_elevation"].tolist()
-        df = str(df[0]+str("sata"))
-        return df
+        cur.execute(f"select reference_information from sites where site_number = '{value}'")
+        df = pd.DataFrame(cur.fetchall(),columns=['reference_information'])
+        reference_information = df["reference_information"].tolist()
+        reference_information = str(reference_information[0])
+        #df = df.append("other reference") 
+
+        #return df
+# reference_elevation
+# reference_elevation
+    cur.execute(f"select reference_elevation from sites where site_number = '{value}'")
+    df = pd.DataFrame(cur.fetchall(),columns=['reference_elevation'])
+    reference_elevation = df["reference_elevation"].tolist()
+    reference_elevation = str(reference_elevation[0])
+
+# reference_inforation
+# reference_inforation
+    cur.execute(f"select reference_information from sites where site_number = '{value}'")
+    df = pd.DataFrame(cur.fetchall(),columns=['reference_information'])
+    reference_information = df["reference_information"].tolist()
+    reference_information = str(reference_information[0])
+
+# observation
+    observation = ""
+
+# output
+    output = f"site: {value}, elevation: {reference_elevation}, elevation of: {reference_information}, observation: {observation}"
+
+    return reference_elevation, reference_information, observation, output
+
