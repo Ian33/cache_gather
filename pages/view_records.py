ad57957723229374b5e2fd6421a97fbcb2f084c4
Ian R. Higgins <ianrhiggins@gmail.com>
2022-08-28 11:12:35 -0700

    updated datatable

--- pages/view_records.py
+++ pages/view_records.py
@@ -1,4 +1,4 @@
-from dash import Dash, Input, Output, callback, dash_table, html, dcc
+from dash import Dash, Input, Output, callback, dash_table, html, dcc, State
 import dash
 import pandas as pd
 import dash_bootstrap_components as dbc
@@ -8,40 +8,43 @@ import psycopg2
 
 dash.register_page(__name__)
 
-#app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-try:
-    DATABASE_URL = os.environ['DATABASE_URL']
-    #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    engine = create_engine(DATABASE_URL)
-except:
-    username = "cwtcmzqujpmszj"
-    password = "d9717d1ad9420277ea4b7a2332885a6e7cbc39073d29a0cfd9f733d2df6835b6"
-    host_name = "ec2-44-206-197-71.compute-1.amazonaws.com"
-    db_name = "d3fpg63d3qj0lt"
-    #conn = psycopg2.connect(f"postgresql://{username}:{password}@{host_name}/{db_name}")
-    engine = create_engine(f"postgresql://{username}:{password}@{host_name}/{db_name}")
-#df = {'site': ["test two"], 'datetime': ["03/20/2022 11:56"], 'observation': ["4"], 'notes': ["a test note"]}
-#df = pd.DataFrame(data=df)
+# info on updating
+# https://dash.plotly.com/live-updates
 
-# Query the database and obtain data as Python objects
-conn = engine.raw_connection()
-cur = conn.cursor()
-cur.execute("SELECT * FROM field_observations;")
-df = pd.DataFrame(cur.fetchall(), columns=['site', 'datetime', 'observation', 'notes'])
+# set up database connection
+from get_field_observations import get_db_obs, get_engine
 
-
-layout = html.Div([dbc.Container([
-    dash_table.DataTable(df.to_dict('records'),[{"name": i, "id": i} for i in df.columns], 
-        id='tbl', sort_action='native', editable=True, row_deletable=True, style_data={
-        'whiteSpace': 'normal',
-        'height': 'auto',
-    },),
-]),
+# db_obs.to_dict('records'),[{"name": i, "id": i} for i in db_obs.columns]
+#db_obs = get_db_obs() 
+layout = html.Div([
+   
+   
 #     html.Div([
-        html.Button('upload', id='upload-val', n_clicks=0),
+        html.Button('load field observations', id='load_val', n_clicks=0),
+        html.Div(id='load_text',
+             children='load'),
+        html.Button('upload', id='upload_val', n_clicks=0),
         html.Div(id='upload_text',
-             children='submit changes')
+             children='submit changes'),
+        html.Div(dash_table.DataTable(id='tbl', sort_action='native', editable=True, row_deletable=True, 
+            style_table={'overflowX': 'auto'},
+            style_cell={
+                'height': 'auto',
+                # all three widths are needed
+                'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+                'whiteSpace': 'normal'}
+
+        )),
+
     ])
+
+
+
+
+
+
+
+
 #])
 """
 @callback(Output('tbl_out', 'children'), Input('tbl', 'active_cell'))
@@ -54,10 +57,31 @@ def update_graphs(active_cell):
     return str(active_cell) if active_cell else "Click the table"
 """
 @callback(
+    Output('tbl', 'data'),
+    Output('tbl', 'columns'),
+    Output('load_text', 'children'),
+    Input('load_val', 'n_clicks'),
+)
+def get_data(load_val):
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    #today = pd.to_datetime("today")
+    if 'load-val' in changed_id:
+        db_obs = get_db_obs()
+        return  db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns], "query"
+    else:
+        db_obs = get_db_obs()
+        return  db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns], "query"
+    #else:
+    #    return  [{}], [], "query"
+    #db_obs = get_db_obs()
+    #return db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns]
+
+@callback(
     Output('upload_text', 'children'),
-    Input('upload-val', 'n_clicks'),
+    Input('upload_val', 'n_clicks'),
     Input('tbl', 'data'),
     Input('tbl', 'columns'),
+    
 )
 def update_output(n_clicks, data, columns):
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
@@ -67,11 +91,10 @@ def update_output(n_clicks, data, columns):
         if df.empty:
             return "no data"
         else:
+            engine = get_engine()
             #df.to_sql('field_observations', cur, if_exists='append')
-            df.to_sql('field_observations', engine, if_exists='append',index=False)
+            #df.to_sql('field_observations', engine, if_exists='append',index=False)
+            df.to_sql('field_observations', engine, if_exists='replace',index=False)
             return "edits submitted"
     else:
         return dash.no_update
-    
-    
-    
