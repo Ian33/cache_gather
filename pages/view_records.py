a14168523675bbd736b9d03243f71cbfd3defe6f
Ian R. Higgins <ianrhiggins@gmail.com>
2022-09-01 21:16:53 -0700

    created sql function and compressed file structure

--- pages/view_records.py
+++ pages/view_records.py
@@ -12,13 +12,9 @@ dash.register_page(__name__)
 # https://dash.plotly.com/live-updates
 
 # set up database connection
-from get_field_observations import get_db_obs, get_engine
+from sql import get_observations, update_observations
 
-# db_obs.to_dict('records'),[{"name": i, "id": i} for i in db_obs.columns]
-#db_obs = get_db_obs() 
 layout = html.Div([
-   
-   
 #     html.Div([
         html.Button('load field observations', id='load_val', n_clicks=0),
         html.Div(id='load_text',
@@ -33,29 +29,9 @@ layout = html.Div([
                 # all three widths are needed
                 'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
                 'whiteSpace': 'normal'}
-
         )),
-
     ])
 
-
-
-
-
-
-
-
-#])
-"""
-@callback(Output('tbl_out', 'children'), Input('tbl', 'active_cell'))
- Output('datatable-upload-container', 'data'),
-    Output('datatable-upload-container', 'columns'),
-return df_csv.to_dict('records'), [{"name": i, "id": i} for i in df_csv.columns]
-        if contents is None:  # nothing to upload
-            return [{}], []
-def update_graphs(active_cell):
-    return str(active_cell) if active_cell else "Click the table"
-"""
 @callback(
     Output('tbl', 'data'),
     Output('tbl', 'columns'),
@@ -65,36 +41,28 @@ def update_graphs(active_cell):
 def get_data(load_val):
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
     #today = pd.to_datetime("today")
-    if 'load-val' in changed_id:
-        db_obs = get_db_obs()
+    if 'load_val' in changed_id:
+        db_obs = get_observations()
         return  db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns], "query"
     else:
-        db_obs = get_db_obs()
+        db_obs = get_observations()
         return  db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns], "query"
-    #else:
-    #    return  [{}], [], "query"
-    #db_obs = get_db_obs()
-    #return db_obs.to_dict('records'), [{"name": i, "id": i} for i in db_obs.columns]
-
+    
 @callback(
     Output('upload_text', 'children'),
     Input('upload_val', 'n_clicks'),
     Input('tbl', 'data'),
     Input('tbl', 'columns'),
-    
 )
 def update_output(n_clicks, data, columns):
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
     #today = pd.to_datetime("today")
-    if 'upload-val' in changed_id:
+    if 'upload_val' in changed_id:
         df = pd.DataFrame(data)
         if df.empty:
             return "no data"
         else:
-            engine = get_engine()
-            #df.to_sql('field_observations', cur, if_exists='append')
-            #df.to_sql('field_observations', engine, if_exists='append',index=False)
-            df.to_sql('observations', engine, if_exists='replace',index=False)
-            return "edits submitted"
+            statement = update_observations(df)
+            return statement
     else:
         return dash.no_update
---
+++ sql.py
@@ -0,0 +1,66 @@
+import os 
+from sqlalchemy import create_engine
+import psycopg2
+import pandas as pd
+
+
+def create_connection():
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        engine = create_engine(DATABASE_URL)
+    except:
+        username = "cwtcmzqujpmszj"
+        password = "d9717d1ad9420277ea4b7a2332885a6e7cbc39073d29a0cfd9f733d2df6835b6"
+        host_name = "ec2-44-206-197-71.compute-1.amazonaws.com"
+        db_name = "d3fpg63d3qj0lt"
+        #conn = psycopg2.connect(f"postgresql://{username}:{password}@{host_name}/{db_name}")
+        engine = create_engine(f"postgresql://{username}:{password}@{host_name}/{db_name}")
+
+    return engine 
+
+def get_site_list():
+    try:
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    except:
+        username = "cwtcmzqujpmszj"
+        password = "d9717d1ad9420277ea4b7a2332885a6e7cbc39073d29a0cfd9f733d2df6835b6"
+        host_name = "ec2-44-206-197-71.compute-1.amazonaws.com"
+        db_name = "d3fpg63d3qj0lt"
+        conn = psycopg2.connect(f"postgresql://{username}:{password}@{host_name}/{db_name}")
+    cur = conn.cursor()
+    cur.execute("select site_number from sites")
+    #df = pd.DataFrame(cur.fetchall())
+    df = pd.DataFrame(cur.fetchall(),columns=['site_number'])
+    df = df["site_number"].to_list()
+    conn.close()
+    return df
+
+def upload_observation(datetime, reference_elevation, observation, site, notes):
+    engine = create_connection()    
+    try:
+        math = float(reference_elevation)-float(observation)
+    except:
+        math=""
+    df = {'site': [site], 'datetime': [datetime], 'observation': [math], 'notes': [notes]}
+    df = pd.DataFrame(data=df)
+    #df.to_sql('field_observations', cur, if_exists='append')
+    df.to_sql('observations', engine, if_exists='append',index=False)
+
+    df = "submitted"
+    return df
+
+def get_observations():
+    engine = create_connection()
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM observations;")
+    db_obs = pd.DataFrame(cur.fetchall(), columns=['site', 'datetime', 'observation', 'notes'])
+    return db_obs
+
+def update_observations(df):
+    engine = create_connection(df)        
+    df.to_sql('observations', engine, if_exists='replace',index=False)
+    statement = "updated"
+    return statement
